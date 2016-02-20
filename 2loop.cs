75473c54a3e036098a72a6e5cb47321f895f74f4
[suiso] <[suiso.banana@gmail.com]>
2016-02-20 17:08:58 +0900

    practice2

---
+++ 2loop.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class RepeatN {
+  static void Main() {
+    Console.WriteLine("数字を入れてください");
+    int input = int.Parse(Console.ReadLine());
+
+    for (int i = 1; i < input + 1; i = i + 1) {
+      Console.WriteLine(new String('*',i));
+    }
+    for (int j = input; j > 0; j -=1) {
+      Console.WriteLine(new String('*',j));
+    }
+  }
+}
