3c68f0883c7ae18534ef5b7d290413d6909eb642
[suiso] <[suiso.banana@gmail.com]>
2016-02-21 17:09:36 +0900

    Rewright

--- csPractice_01.cs
+++ csPractice_01.cs
@@ -6,8 +6,13 @@ public class RepeatN {
   static void Main() {
     Console.WriteLine("数字を入れてください");
     var input = int.Parse(Console.ReadLine());
-    for (int i = 1; i < input + 1; i = i + 1) {
-      Console.WriteLine(new String('*',i));
+    string kome = "";
+
+    for (int i = 0; i < input; i = i + 1) {
+      kome += "*";
+      Console.WriteLine(kome);
+
+      //Console.WriteLine(new String('*',i));
       }
     }
   }
